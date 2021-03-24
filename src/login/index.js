b7d293fe15f1e0119100aa32bb876a874c8ff871
Trevan Reese <trevanreeser41@gmail.com>
2021-03-24 12:20:57 -0600

    change login image

--- src/login/index.js
+++ src/login/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { LoginForm } from "./form"
 import logo from "../resources/images/logo.svg"
-import hero_img from "../resources/images/blue-hero.jpg"
+import hero_img from "../resources/images/login.jpeg"
 
 export function Login(props) {
     return (
@@ -9,7 +9,10 @@ export function Login(props) {
             <div className="flex-1 flex flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
                 <div className="mx-auto w-full max-w-sm lg:w-96">
                     <div>
-                        <img className="h-12 w-auto" src={logo} alt="realium-logo"/>
+                        <a href="/">
+                            <span className="sr-only">Workflow</span>
+                            <img className="h-12 w-auto sm:h-16" src={logo} alt="Realium"/>
+                        </a>
                         <h2 className="mt-6 text-3xl font-extrabold text-gray-900">
                             Sign in to your account
                         </h2>
