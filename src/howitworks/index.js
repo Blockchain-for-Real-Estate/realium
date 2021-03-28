3c19719adc2ef6d8ce3679191cef6ffd1a6bfc60
Trevan Reese <trevanreeser41@gmail.com>
2021-03-27 18:14:36 -0600

    test ci/cd to s3

--- src/howitworks/index.js
+++ src/howitworks/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Hero } from "../utilities/hero"
 import hero_img from "../resources/images/orange-hero.jpg"
 
-export function HowItWorks(props) {
+export function HowItWorks() {
     return (
         <Hero img={hero_img} page="howitworks" />
     )
