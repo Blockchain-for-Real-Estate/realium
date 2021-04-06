3280f68d2b4bbe143a1393211ba7a03d7d90501e
Trevan Reese <trevanreeser41@gmail.com>
2021-04-05 21:16:39 -0600

    tune auth token header

--- src/api/services/event.service.ts
+++ src/api/services/event.service.ts
@@ -30,11 +30,7 @@ export class ApiEventService {
 
     public postTransaction(data: Event, token: string) {
       const headers = {
-<<<<<<< HEAD
         "Authorization": "Token " + token,
-=======
-        "HTTP_AUTHORIZATION": "Token " + token
->>>>>>> be99058 (trim payload)
       }
       return axios.post<Event>(
         `${environment.api}/api/events/`,
